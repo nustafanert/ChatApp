69271f44e563a0981f85955587d0df2412335854
Mustafa Mert Altınsoy <mustafamertaltinsoyedu@gmail.com>
2025-01-16 14:54:01 +0300

    main

---
+++ app/src/main/java/com/unty/chat/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.unty.chat.listeners;
+
+import com.unty.chat.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/unty/chat/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.unty.chat.listeners;
+
+import com.unty.chat.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
